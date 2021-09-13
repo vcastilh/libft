05f0ceafa7e9e0a0c61ea883ec5bc81c0111964d
vcastilh <viniciaofelix@gmail.com>
2021-09-13 10:13:46 -0300

    Added handle for /0 character int the middle of string

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 18:16:37 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/09/04 00:23:57 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/09/13 08:51:57 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t len)
 	i = 0;
 	while (len--)
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] == s2[i] && s1[i] && s2[i])
 			i++;
 		else
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
